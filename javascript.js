fbe7c3ee3d828f4bc3a8917b20e6950e8f837bef
4lokym <mykola.koshilko@gmail.com>
2025-03-12 20:01:03 +0100

    Bugfix: only one book displayed
    I've made sure that every time a new cloned the book_card template node
    is displayed and not the same node is being modified.

--- javascript.js
+++ javascript.js
@@ -25,22 +25,35 @@ function display_book(book){
         throw new Error("Cannot display a non book object, insert a book object");
     }
 
-    book_div.id = book.id;
+    book_div_copy = book_div.cloneNode(true);
+    book_div_copy.id = book.id;
 
-    const title = book_div.querySelector(".title");
+    const title = book_div_copy.querySelector(".title");
     title.textContent += ` ${book.title}`;
 
-    const author = book_div.querySelector(".author");
+    const author = book_div_copy.querySelector(".author");
     author.textContent += ` ${book.author}`;
 
-    const pages = book_div.querySelector(".pages");
+    const pages = book_div_copy.querySelector(".pages");
     pages.textContent += ` ${book.pages}`;
 
-    const read = book_div.querySelector(".read");
+    const read = book_div_copy.querySelector(".read");
     read.textContent += ` ${book.read}`;
     
-    document.querySelector("#center-content").appendChild(book_div);
+    document.querySelector("#center-content").appendChild(book_div_copy);
 }
+
+function display_books(){
+    myLibrary.forEach(element => {
+        display_book(element);
+    });
+}
+
+
+//test
+addBooktoLibrary("a", "t", 35, true);
+addBooktoLibrary("b", "t", 35, true);
+display_books();
 
 
 
